b8f23908f20854b4ae640629946be09209b805d9
Piotr Plucinski <plutocamera@gmail.com>
2025-01-27 20:37:32 +0100

    pattern matching

---
+++ python_snipets/pattern_matching.py
@@ -0,0 +1,63 @@
+# simple match
+from collections.abc import Callable
+
+
+def ex_1():
+    while True:
+        command = input('Enter command: \n')
+        match command:
+            case "add todo":
+                print(f"ToDo added!")
+            case "show todos":
+                print('yours todos')
+            case "bye":
+                print('bye bye !')
+                break
+
+            case _:
+                print("unknown command")
+
+
+# match on different types
+def ex_2():
+    for x in 1, 'two', 3.14, Exception('yolo'), lambda data: data + 42, [1, 2, 3], (1, 2, 3):
+        match x:
+            case int():
+                print(f"int: {x}")
+            case str():
+                print(f"str: {x}")
+            case float():
+                print(f"float: {x}")
+            case Exception():
+                print(f"Exception: {x}")
+            case Callable():
+                print(f"callable: {x}")
+            case y:
+                print(f"unknown: {y}")
+
+
+# guarded matching
+def ex_3():
+    for i in range(6):
+        match i:
+            case 1 | 3 | 5 if i < 4:
+                print(f"{i} is odd and less then 4")
+            case 1 | 3 | 5 if i > 4:
+                print(f"{i} is odd and greater than 4")
+            case 2 | 4:
+                print(f"{i} is even")
+
+
+def ex_4():
+    for calls in [1,2,3], (4,5,6), {"a":42}:
+        match calls:
+            case [1,2,3] as my_list:
+                print(f"list: {my_list}")
+            case (_,5 as my_int,_) as my_tupe:
+                print(f"tuple: {my_tupe} with {my_int}")
+            case {"a": 42} as my_dict:
+                print(f"dict: {my_dict}")
+            case _:
+                print(f"unknown: {calls}")
+
+ex_4()
